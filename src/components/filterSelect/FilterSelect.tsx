6fc263ad399041ce7788509ef3745af0cada0e26
Alisson <alissonktt@hotmail.com>
2024-12-14 18:48:42 -0300

    fix: removendo FilterSelect e utilizando searchinput

--- src/components/filterSelect/FilterSelect.tsx
+++ src/components/filterSelect/FilterSelect.tsx
@@ -1,22 +1,23 @@
 import React from "react";
-import { useDispatch } from "react-redux";
-import { sortAppointments } from "../../store/slices/modalSlice";
+
+
 
 const FilterSelect: React.FC = () => {
-  const dispatch = useDispatch();
+ 
 
-  const handleSortChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    const sortBy = event.target.value as "index" | "name";
-    dispatch(sortAppointments(sortBy));
-  };
+  // const handleSortChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+  //   const sortBy = event.target.value as "index" | "name";
+  //   dispatch(sortAppointments(sortBy));
+  // };
 
   return (
-    <div>
-      <select onChange={handleSortChange}>
-        <option value="index">Ordenar por Índice</option>
-        <option value="name">Ordenar por Nome</option>
-      </select>
-    </div>
+    // <div>
+    //   <select onChange={handleSortChange}>
+    //     <option value="index">Ordenar por Índice</option>
+    //     <option value="name">Ordenar por Nome</option>
+    //   </select>
+    // </div>
+    <></>
   );
 };
 
